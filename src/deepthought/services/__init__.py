2c18fa3ed704f632963ef161968a610592568e08
Tino Tonitini <georgepike0@gmail.com>
2025-06-30 14:28:59 -0400

    Add SocialGraphService and tests

--- src/deepthought/services/__init__.py
+++ src/deepthought/services/__init__.py
@@ -1,8 +1,15 @@
 """Service utilities for DeepThought."""
 
 from .file_graph_dal import FileGraphDAL
-from .memory_service import MemoryService
 from .hierarchical_service import HierarchicalService
+from .memory_service import MemoryService
 from .persona_manager import PersonaManager
+from .social_graph_service import SocialGraphService
 
-__all__ = ["FileGraphDAL", "MemoryService", "HierarchicalService", "PersonaManager"]
+__all__ = [
+    "FileGraphDAL",
+    "MemoryService",
+    "HierarchicalService",
+    "PersonaManager",
+    "SocialGraphService",
+]
