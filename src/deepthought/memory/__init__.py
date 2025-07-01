9a6c6438bc926ed0d104178b752c983b12f45d90
Tino Tonitini <georgepike0@gmail.com>
2025-07-01 09:56:05 -0400

    Add offline search and FaissVectorStore tests

--- src/deepthought/memory/__init__.py
+++ src/deepthought/memory/__init__.py
@@ -1,5 +1,6 @@
 """Memory utilities."""
 
+from .faiss_vector_store import FaissVectorStore
 from .hierarchical import HierarchicalMemory
 from .tiered import TieredMemory
 from .vector_store import SimpleEmbeddingFunction, VectorStore, create_vector_store
@@ -7,6 +8,7 @@ from .vector_store import SimpleEmbeddingFunction, VectorStore, create_vector_st
 __all__ = [
     "HierarchicalMemory",
     "VectorStore",
+    "FaissVectorStore",
     "create_vector_store",
     "SimpleEmbeddingFunction",
     "TieredMemory",
